46ab73c19893da03ef0b6f1eff32cf09fb440364
Xiaoguang Li <lixiaoguang@gmail.com>
2025-06-15 09:00:23 -0400

    ann

--- edu/01-learn-neural-networks-from-scratch/py/01-artificial-neural-networks/01-linear-prediction.py
+++ edu/01-learn-neural-networks-from-scratch/py/01-artificial-neural-networks/01-linear-prediction.py
@@ -1,15 +1,16 @@
 import numpy as np
 
-# dataset
+# 输入数据
 feature = np.array([28.1, 58.0])
 
-# model
-weight, bias = np.array([1.0, 1.0]), np.array([0.0])
+# 模型参数（权重和偏置）
+weight, bias = np.array([1, 1]), np.array([0])
 
-# neuron
+# 神经元逻辑
 forward = lambda x: x.dot(weight.T) + bias
 
-# predict
+# 模型推理
 prediction = forward(feature)
 
-print(f'Prediction: {prediction}')
+# 结果输出
+print(f'预测冰淇淋销量：{prediction}')
