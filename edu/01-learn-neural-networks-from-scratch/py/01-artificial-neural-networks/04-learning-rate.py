18aaeab583028e1f586c94aac64bf89a67a13906
Xiaoguang Li <lixiaoguang@gmail.com>
2025-06-18 19:14:03 -0400

    ann

---
+++ edu/01-learn-neural-networks-from-scratch/py/01-artificial-neural-networks/04-learning-rate.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+# 输入数据
+feature = np.array([28.1, 58.0])
+# 实际结果
+label = np.array([165])
+
+# 模型参数（权重和偏置）
+weight, bias = np.array([[1, 1]]) / 2, np.array([0])
+
+
+# 神经元逻辑（线性回归（多元一次）函数）
+def forward(x, w, b):
+    return x.dot(w.T) + b
+
+
+# 反向传播
+def backward(x, d, w, b, lr):
+    return w - d * x * lr, b - np.sum(d, axis=0) * lr
+
+
+# 损失函数（均方差）
+def mse_loss(p, y):
+    return ((p - y) ** 2).mean()
+
+
+# 梯度计算（损失函数的微分）
+def gradient(p, y):
+    return (p - y) * 2 / len(y)
+
+
+# 学习率
+LEARNING_RATE = 0.00001
+
+# 模型推理
+prediction = forward(feature, weight, bias)
+# 模型验证
+error = mse_loss(prediction, label)
+# 梯度计算
+delta = gradient(prediction, label)
+# 反向传播
+weight, bias = backward(feature, delta, weight, bias, LEARNING_RATE)
+# 结果输出
+print(f'预测冰淇淋销量：{prediction}')
+print(f'均方差：{error}')
+print(f"权重：{weight}")
+print(f"偏置：{bias}")
