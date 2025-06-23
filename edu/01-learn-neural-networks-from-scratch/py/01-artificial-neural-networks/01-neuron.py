0bbbf700915b45d16451c48273a2712a702d6a1e
Xiaoguang Li <lixiaoguang@gmail.com>
2025-06-22 23:41:53 -0400

    ann

--- edu/01-learn-neural-networks-from-scratch/py/01-artificial-neural-networks/01-neuron.py
+++ edu/01-learn-neural-networks-from-scratch/py/01-artificial-neural-networks/01-neuron.py
@@ -1,18 +1,19 @@
 import numpy as np
 
-# 输入数据
-feature = np.array([28.1, 58.0])
+# 输入特征
+features = np.array([28.1, 58.0])
 
-# 模型参数（权重和偏置）
-weight, bias = np.array([[1, 1]]) / 2, np.array([0])
+# 初始化模型参数
+weights = np.array([[0.5, 0.5]])
+bias = np.array([0])
 
 
-# 神经元逻辑（线性回归（多元一次）函数）
+# 前向传播函数
 def forward(x, w, b):
     return x.dot(w.T) + b
 
 
 # 模型推理
-prediction = forward(feature, weight, bias)
-# 结果输出
-print(f'预测冰淇淋销量：{prediction}')
+prediction = forward(features, weights, bias)
+# 输出结果
+print(f'预测的冰淇淋销量为: {prediction}')
